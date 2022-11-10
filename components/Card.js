a2cb8375ff3a1d9dd6ff9bda377dfcb2263f832e
TrikKH <ejames711@gmail.com>
2022-11-09 20:02:22 -0600

    add count to card

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,38 @@
+import Image from "next/image";
+import { useState } from "react";
 
-export default function Card({description, image}){
+export default function Card({name,description,image}){
+
+    const [count, setCount] = useState(0)
+
+    function addCount() {
+        if (count < 100) {
+            setCount(count + 1)
+        }
+        else {
+            setCount(100)
+        }
+    }
+
+    function subCount() {
+        if (count  > 0) {
+            setCount(count - 1)
+        } else {
+            setCount(0)
+        }
+    }
+
     return(
-        <div className="flex flex-col items-center ml-10 text-center rounded shadow-md justify-evenly w-52 h-80 bg-light_blue">
-            <div className="w-2/3 bg-white rounded h-1/3"></div>
-            <h1 className="text-xl">Pokeball</h1>
-            <p className="text-sm">The classic. Always there when you need it</p>
+        <div className="flex flex-col items-center text-center rounded shadow-md justify-evenly w-52 h-80 bg-light_blue">
+            <div className="w-2/3 bg-white rounded h-1/3">
+                <Image src={""} height={40} width={40} />
+            </div>
+            <h1 className="text-xl">{name}</h1>
+            <p className="text-sm">{description}</p>
             <div className="flex items-center w-full justify-evenly">
-                <button className="w-1/6 h-6 bg-white rounded-md">-</button>
-                <div className="w-1/3 h-6 bg-white rounded-md">0</div>
-                <button className="w-1/6 h-6 bg-white rounded-md">+</button>
+                <button className="w-1/6 h-6 bg-white rounded-md" onClick={subCount}>-</button>
+                <div className="w-1/3 h-6 bg-white rounded-md">{count}</div>
+                <button className="w-1/6 h-6 bg-white rounded-md" onClick={addCount}>+</button>
             </div>
         </div>
     )
--- components/CardDisplay.js
+++ components/CardDisplay.js
@@ -2,7 +2,7 @@ import Card from './Card'
 
 export default function CardDisplay() {
     return(
-        <div className='flex justify-center'>
+        <div className='flex justify-center gap-4 my-6'>
             <Card />
         </div>
     )
